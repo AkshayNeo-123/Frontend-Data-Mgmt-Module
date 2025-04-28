f5d779f73f809f53d3a2d2945572b7bb3a4968ac
AkshayNeo-123 <akshay.godbole@neosoftmail.com>
2025-04-28 10:13:20 +0530

    dashboard changes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,25 +1,3 @@
-// // src/app/services/auth.service.ts
-
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthService {
-//   getLoggedInUser() {
-//     throw new Error('Method not implemented.');
-//   }
-//   private apiUrl = 'https://localhost:7030/api/Account';
-
-//   constructor(private http: HttpClient) {}
-
-//   login(userName: string, password: string): Observable<any> {
-//     return this.http.post(`${this.apiUrl}/login`, { userName, password }, { responseType: 'text' });
-//   }
-// }
-
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -28,7 +6,7 @@ import { Observable } from 'rxjs';
 export interface LoggedInUser {
   name: string;
   email?: string;
-  // add other fields as needed
+ 
 }
 
 @Injectable({
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,8 +1,9 @@
 // src/app/services/recipe.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 import { Recipe } from '../models/recipe.model';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,13 @@ export class RecipeService {
 
   constructor(private http: HttpClient) {}
 
+ 
   getAllRecipes(): Observable<Recipe[]> {
-    return this.http.get<Recipe[]>(`${this.baseUrl}/GetAll`);
+    return this.http.get<Recipe[]>(`${this.baseUrl}/GetAll`).pipe(
+      catchError(error => {
+        console.error('Error fetching recipes:', error);
+        return of([]);
+      })
+    );
   }
 }
