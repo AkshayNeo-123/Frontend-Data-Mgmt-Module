04176351f466d4245ea71ec39489aeb0e273d915
kaps2607 <kapillund29@gmail.com>
2025-05-22 15:25:56 +0530

    mergedAkshay

--- src/app/dashboard/sidebar/sidebar.component.ts
+++ src/app/dashboard/sidebar/sidebar.component.ts
@@ -5,6 +5,7 @@ import { Menu } from '../../models/menu.model';
 import { PermissionServiceService } from '../../services/permission-service.service';
 import { Router, RouterModule, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { RoleService } from '../../services/role.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -14,11 +15,18 @@ import { filter } from 'rxjs/operators';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
+  roleId: number = 0;
+  allMenus: Menu[] = [];
+  allowedMenus: Menu[] = [];
   @Input() isOpen: boolean = true;
   isSidebarOpen = true;
   activeMenu: string = '';
-
-  constructor(private router: Router) {
+  permissions: any[] = [];
+  constructor(
+    private router: Router,
+    private roleService: RoleService,
+    private permissionService: PermissionServiceService
+  ) {
     // Listen to navigation changes
     this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
@@ -46,5 +54,38 @@ export class SidebarComponent {
 
   onToggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
+  }
+  canView(menuName: string): boolean {
+  return this.permissions.some(p => p.menuName === menuName && p.canView);
+}
+ngOnInit() {
+    const roleId = Number(localStorage.getItem('RoleId'));
+    this.roleService.getRoleById(roleId).subscribe({
+      next: (res) => {
+        console.log(res)
+        const permissions = res.rolePermissions;
+
+        this.permissionService.setPermissions(permissions);
+
+        const allowedMenuIds = permissions
+          .filter((p: any) => p.canView)
+          .map((p: any) => p.menuId);
+
+        this.roleService.getAllMenus().subscribe({
+          next: (menus) => {
+            this.allMenus = menus;
+            
+            console.log(this.allMenus)
+            this.allowedMenus = this.allMenus.filter(menu =>
+              allowedMenuIds.includes(menu.id)
+              
+            );
+            console.log('hi',this.allowedMenus);
+            
+          
+          }
+        });
+      }
+    });
   }
 }
