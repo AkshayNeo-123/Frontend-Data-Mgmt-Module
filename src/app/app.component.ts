2b6db7adda58c530cf50e6a074f3fe70ad603dd7
AkshayNeo-123 <akshay.godbole@neosoftmail.com>
2025-05-27 16:24:20 +0530

    toggle side bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
 import { TopbarComponent } from './dashboard/topbar/topbar.component';
-import { SidebarComponent } from "./dashboard/sidebar/sidebar.component";
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { AuthService } from './services/auth.service';
 import { filter } from 'rxjs';
@@ -11,50 +11,61 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule,RouterOutlet, MatTableModule,TopbarComponent, SidebarComponent,FooterComponent, CommonModule],
+  imports: [
+    FormsModule,
+    RouterOutlet,
+    MatTableModule,
+    TopbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  standalone:true
+  standalone: true,
 })
 export class AppComponent {
   title = 'data-management-ui';
   currentRoute: string = '';
 
-    constructor(private router: Router,private authService: AuthService) {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: NavigationEnd) => {
-      this.currentRoute = event.urlAfterRedirects;
-    });
+  isSidebarOpen = true;
+
+  constructor(private router: Router, private authService: AuthService) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute = event.urlAfterRedirects;
+      });
   }
 
-    get isStartPage(): boolean {
+  get isStartPage(): boolean {
     return this.currentRoute === '/';
   }
- 
+
   isCurrentRoute(routes: string[]): boolean {
     return routes.includes(this.currentRoute);
   }
- 
+
   get needSideBar(): boolean {
-    return this.isCurrentRoute([ '/login','/']);
+    return this.isCurrentRoute(['/login', '/']);
   }
   // isSidebarOpen = true;
   // onToggleSidebar(): void {
   //   this.isSidebarOpen = !this.isSidebarOpen;
   // }
-  
- 
- 
+
   get showSidebar(): boolean {
     return !this.isStartPage && !this.needSideBar;
   }
- 
+
   get showHeaderAndFooter(): boolean {
-    return !this.isStartPage
+    return !this.isStartPage;
   }
   get showBackButton(): boolean {
-    return !this.isCurrentRoute(['/login','/']);
+    return !this.isCurrentRoute(['/login', '/']);
   }
- 
+
+  onToggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
